043c815c58cd759c96f0ccaf249068923a702768
Sonya Vpaa <sonja.vpaa@gmail.com>
2022-03-29 13:39:27 +0300

    modified class.modules functionalities

--- src/Main.js
+++ src/Main.js
@@ -22,19 +22,26 @@ class Main extends Component {
   };
 
   render() {
+    let classStyle;
+    if (this.state.counter === 0) {
+      classStyle = `${classes.start}`;
+    } else if (this.state.counter % 2 === 0) {
+      classStyle = `${classes.evencolor}`;
+    } else {
+      classStyle = `${classes.oddcolor}`;
+    }
+
     return (
-      <div className={classes.main}>
-        <div className={classes.mainContainer}>
-          <div className={classes.circle}>
-            <p className="sum">{this.state.counter}</p>
-          </div>
-          <div className={classes.buttonContainer}>
-            <Button text="Add five" value="5" click={this.addValue} />
-            <Button text="Add one" value="1" click={this.addValue} />
-            <Button text="Remove" click={this.reset} />
-            <Button text="Remove one" value="-1" click={this.addValue} />
-            <Button text="Remove five" value="-5" click={this.addValue} />
-          </div>
+      <div className={`${classes.mainContainer} ${classes.main}`}>
+        <div className={` ${classes.circle} ${classStyle}`}>
+          <p>{this.state.counter}</p>
+        </div>
+        <div className={classes.buttonContainer}>
+          <Button text="Add five" value="5" click={this.addValue} />
+          <Button text="Add one" value="1" click={this.addValue} />
+          <Button text="Remove" click={this.reset} />
+          <Button text="Remove one" value="-1" click={this.addValue} />
+          <Button text="Remove five" value="-5" click={this.addValue} />
         </div>
       </div>
     );
